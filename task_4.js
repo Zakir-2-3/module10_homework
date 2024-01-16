64e98552fc0d1857c61e1ca1c46616415b57889d
Zakir <112572164+Zakir-2-3@users.noreply.github.com>
2024-01-16 11:27:17 +0300

    task_4.js

--- task_4.js
+++ task_4.js
@@ -2,6 +2,6 @@
 
 // Записать в переменную случайное целое число в диапазоне [0; 100]. Используйте объект Math.
 
-let randomNumber = Math.floor(Math.random() * 101);
+const randomNumber = Math.floor(Math.random() * 101);
 
 console.log(randomNumber);
